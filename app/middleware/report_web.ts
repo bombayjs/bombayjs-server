0e14a1f79e28ce7518ece505e8de66f114d8bccf
hdl <1158025556@qq.com>
2019-09-29 10:00:47 +0800

    修改响应时间

--- app/middleware/report_web.ts
+++ app/middleware/report_web.ts
@@ -1,5 +1,8 @@
+const moment = require('moment');
+
 module.exports = options => {
   return async (ctx, next) => {
+    const startTime = moment();
     const find =  options.match.find(item=>ctx.url.includes(item))
     if(find){
       const { err }:{err: Object} =  JSON.parse(ctx.request.body)||{}
@@ -12,7 +15,7 @@ module.exports = options => {
     if(find){
       ctx.set('Access-Control-Allow-Origin', '*');
       ctx.set('Content-Type', 'application/json;charset=UTF-8');
-      ctx.set('X-Response-Time', '2s');
+      ctx.set('X-Response-Time', moment().diff(startTime))
       ctx.set('Connection', 'close');
     }
   }
