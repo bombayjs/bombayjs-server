5d6e0488cea5185920d246461b86baad7f5e3a61
hdl <1158025556@qq.com>
2019-10-14 18:04:32 +0800

    暂时去除定时任务

--- app/schedule/update_location.ts
+++ app/schedule/update_location.ts
@@ -11,22 +11,22 @@ class UpdateLocation extends Subscription {
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    const res = await this.app.elasticsearch.search({
-      index: 'frontend-event-log-web-report-collect-*',
-      type: '_doc',
-      body: {
-        size: 1,
-        query: {
-          bool: {
-            must_not: {
-              exists: {
-                field: 'ad_info.nation',
-              },
-            },
-          },
-        },
-      },
-    });
+    // const res = await this.app.elasticsearch.search({
+    //   index: 'frontend-event-log-web-report-collect-*',
+    //   type: '_doc',
+    //   body: {
+    //     size: 1,
+    //     query: {
+    //       bool: {
+    //         must_not: {
+    //           exists: {
+    //             field: 'ad_info.nation',
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // });
     // const { _index, _type, _id, _source } = res.hits.hits[0];
     // await this.app.elasticsearch.index({
     //   id: _id,
@@ -34,7 +34,7 @@ class UpdateLocation extends Subscription {
     //   type: _type,
     //   body: _source,
     // });
-    console.dir(res.hits.hits);
+    // console.dir(res.hits.hits);
   }
 }
 
